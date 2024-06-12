0f51e37747aa80ba5315cdac0ce2ba11ba72839d
AhmedFathyMohamed10 <tous5099@gmail.com>
2024-06-12 13:03:23 +0300

    update the settings again to add basic auth and session auth , on the url i added the obtain_auth_token

--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/rest/', include('rest_framework.urls')),
+    path('api/token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/auth/', include('accounts.urls')),
     path('', include('product.urls')),
     path('api/', include('order.urls')),
